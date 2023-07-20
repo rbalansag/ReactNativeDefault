a55a3512350b6aabaf3563822275f236011ef5fb
Rey Balansang <reybalansang@Deployeds-MacBook-Pro.local>
2023-07-20 14:08:48 +0800

    [+] auth0 and app rename

--- ios/reactNativeDefaultTests/reactNativeDefaultTests.m
+++ ios/reactNativeDefaultTests/reactNativeDefaultTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Auth0SamplesTests : XCTestCase
+@interface reactNativeDefaultTests : XCTestCase
 
 @end
 
-@implementation Auth0SamplesTests
+@implementation reactNativeDefaultTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
