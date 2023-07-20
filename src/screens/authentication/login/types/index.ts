498eac84011645c4dcd54607243244c6c0c33690
Rey Balansang <reybalansang@Deployeds-MacBook-Pro.local>
2023-07-20 16:24:21 +0800

    [-] remove duplicate src

--- src/screens/authentication/login/types/index.ts
+++
@@ -1,9 +0,0 @@
-// Interface
-export interface FormValues {
-   email: tEmail;
-   password: tPassword;
-}
-
-// Types
-export type tEmail = string;
-export type tPassword = string;
--- src/screens/landing/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import { tEmail, tKey, tPassword, tValue } from "../types";
-
-// form
-export const email: tEmail = "";
-export const password: tPassword = "";
-
-export const initialKey: tKey = "";
-export const initialValue: tValue = "";
